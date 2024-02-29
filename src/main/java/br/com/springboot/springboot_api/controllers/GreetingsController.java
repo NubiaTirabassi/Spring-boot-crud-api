4365bcee679448d0b8fa280b868a175b7cf701c2
Nubia Tirabassi <nu.ragagnin91@gmail.com>
2024-02-29 17:21:57 -0300

    Conexão com postman e endpoint para salvar e deletar no banco de dados.

--- src/main/java/br/com/springboot/springboot_api/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/springboot_api/controllers/GreetingsController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +60,25 @@ public class GreetingsController {
     	
     	List<Usuario> usuarios = usuarioRepository.findAll();
     	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna lista em JSON*/
+    	
 		}
+    
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar (@RequestBody Usuario usuario) {
+    	Usuario user = usuarioRepository.save(usuario);
+    
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+    
+    @DeleteMapping(value = "deletar")
+    @ResponseBody
+    public ResponseEntity<String> delete (@RequestParam Long iduser) {
+    	usuarioRepository.deleteById(iduser);
+    
+    	return new ResponseEntity<String>("User deletado com sucesso!", HttpStatus.OK);
+    }
+    
+    
 }
+
